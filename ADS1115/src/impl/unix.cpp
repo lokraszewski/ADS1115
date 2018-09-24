034dfebf6c73e90cc8e953266c06d1567a25533c
Lukasz Okraszewski <lukasz.okraszewski@lig-biowise.com>
2018-09-24 08:39:39 +0100

    Added a proper exception with message

--- ADS1115/src/impl/unix.cpp
+++ ADS1115/src/impl/unix.cpp
@@ -2,10 +2,12 @@
  * @Author: Lukasz
  * @Date:   05-09-2018
  * @Last Modified by:   Lukasz
- * @Last Modified time: 06-09-2018
+ * @Last Modified time: 24-09-2018
  */
 
 #include "unix.h"
+#include <exception>
+
 namespace ADS1115
 {
 
@@ -13,8 +15,7 @@ void i2cImpl::begin(uint8_t address) const
 {
   if (ioctl(m_file, I2C_SLAVE, address) < 0)
   {
-    printf("Failed to acquire bus access and/or talk to slave.\n");
-    throw;
+    throw runtime_error("Failed to acquire bus access and/or talk to slave.");
   }
 }
 
@@ -23,8 +24,7 @@ void i2cImpl::write(uint8_t* data, size_t length) const
 
   if (::write(m_file, data, length) != length)
   {
-    printf("Failed to write to the i2c bus.\n");
-    throw;
+    throw runtime_error("Failed to write to the i2c bus.");
   }
 }
 void i2cImpl::read(uint8_t* data, size_t length) const
@@ -32,8 +32,7 @@ void i2cImpl::read(uint8_t* data, size_t length) const
 
   if (::read(m_file, data, length) != length)
   {
-    printf("Failed to read from the i2c bus.\n");
-    throw;
+    throw runtime_error("Failed to read from the i2c bus.");
   }
 }
 } // namespace ADS1115
