296e3679341ef631e28c4c98c528b7a354c93385
Lukasz Okraszewski <lukasz.okraszewski@lig-biowise.com>
2018-09-24 08:40:25 +0100

    Forgot namespace for runtime_error

--- ADS1115/src/impl/unix.cpp
+++ ADS1115/src/impl/unix.cpp
@@ -15,7 +15,7 @@ void i2cImpl::begin(uint8_t address) const
 {
   if (ioctl(m_file, I2C_SLAVE, address) < 0)
   {
-    throw runtime_error("Failed to acquire bus access and/or talk to slave.");
+    throw std::runtime_error("Failed to acquire bus access and/or talk to slave.");
   }
 }
 
@@ -24,7 +24,7 @@ void i2cImpl::write(uint8_t* data, size_t length) const
 
   if (::write(m_file, data, length) != length)
   {
-    throw runtime_error("Failed to write to the i2c bus.");
+    throw std::runtime_error("Failed to write to the i2c bus.");
   }
 }
 void i2cImpl::read(uint8_t* data, size_t length) const
@@ -32,7 +32,7 @@ void i2cImpl::read(uint8_t* data, size_t length) const
 
   if (::read(m_file, data, length) != length)
   {
-    throw runtime_error("Failed to read from the i2c bus.");
+    throw std::runtime_error("Failed to read from the i2c bus.");
   }
 }
 } // namespace ADS1115
